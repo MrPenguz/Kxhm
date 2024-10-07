e1a5609245168f34d12e9d17a6af8d5e3a5916a7
MrPenguz <mohammadmasere@gmail.com>
2024-10-07 18:23:12 +0300

    adding newsletterbox

---
+++ src/components/NewsLetter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsLetter = () => {
+    return (
+        <div className='text-center'>
+            <p className='text-2xl font-medium text-gray-800 py-6'>Subscribe Now & Get 20% Off!</p>
+        </div>
+    )
+}
+
+export default NewsLetter
